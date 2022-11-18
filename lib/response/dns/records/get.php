ec3ac69087e27f97df0dd691a0db8449a99558e6
Del Putnam <del@putnams.net>
2022-11-18 15:32:14 -0500

    Update dns records client code

--- lib/response/dns/records/get.php
+++ lib/response/dns/records/get.php
@@ -11,8 +11,8 @@ class Get implements Response\Response_Interface {
 	use Response\Data_Trait;
 
 	public function get_dns_records(): Entity\Dns_Records {
-		$domain_name_data = $this->get_data_by_key( 'dns_records.domain' );
-		$record_sets_data = $this->get_data_by_key( 'dns_records.record_sets' );
+		$domain_name_data = $this->get_data_by_key( 'data.dns_records.domain' );
+		$record_sets_data = $this->get_data_by_key( 'data.dns_records.record_sets' );
 
 		return Entity\Dns_Records::from_array( $domain_name_data, $record_sets_data );
 	}
--- test/api/api-test.php
+++ test/api/api-test.php
@@ -4,26 +4,8 @@ namespace Automattic\Domain_Services\Test;
 
 use Automattic\Domain_Services\{Api, Command, Configuration, Entity, Mock, Response};
 
-class ApiTest extends \PHPUnit\Framework\TestCase {
+class ApiTest extends Domain_Services_Client_Test_Case {
 	public function test_api_success(): void {
-		$mock_response_array = get_mock_response( 'Domain_Contacts_Set' );
-
-		$mock_response = new Mock\Http\Response();
-		$mock_response->set_mock_body_from_array( $mock_response_array );
-
-		$mock_http_client = new Mock\Http\Client();
-		$mock_http_client->set_mock_response( $mock_response );
-
-		$config = Configuration::get_default_configuration()
-			->set_api_key( 'X-DSAPI-KEY', 'YOUR_API_KEY' )
-			->set_api_key( 'X-DSAPI-USER', 'YOUR_API_USER' );
-
-		$api = new Api(
-			$config,
-			new Response\Factory(),
-			$mock_http_client,
-		);
-
 // Set the domain to use
 		$domain_name = new Entity\Domain_Name( 'a8ctest.com' );
 
@@ -55,15 +37,43 @@ class ApiTest extends \PHPUnit\Framework\TestCase {
 // Create an optional client transaction ID
 		$client_transaction_id = 'client_tx_id_example';
 
-		try {
+        $mock_response_array = get_mock_response( $command, 'success' );
+
+        $mock_response = new Mock\Http\Response();
+        $mock_response->set_mock_body_from_array( $mock_response_array );
+
+        $mock_http_client = new Mock\Http\Client();
+        $mock_http_client->set_mock_response( $mock_response );
+
+        $config = Configuration::get_default_configuration()
+            ->set_api_key( 'X-DSAPI-KEY', 'YOUR_API_KEY' )
+            ->set_api_key( 'X-DSAPI-USER', 'YOUR_API_USER' );
+
+        $api = new Api(
+            $config,
+            new Response\Factory(),
+            $mock_http_client,
+        );
+
+        try {
 			// Make the call to the endpoint
 			/** @var Response\Domain\Contacts\Set $response */
 			$response = $api->post( $command, $client_transaction_id );
 
 			// Extract some data from the resopnse
-			echo "Status: " . $response->get_status() . "\n";
-			echo "Status description: " . $response->get_status_description() . "\n";
-			echo "New contact ID: " . $response->get_contacts()->get_owner()->get_contact_id()->get_provider_contact_id() . "\n";
+            $this->assertEquals( $mock_response_array['status'], $response->get_status() );
+            $this->assertEquals( $mock_response_array['status_description'], $response->get_status_description() );
+            $this->assertEquals( $mock_response_array['status_description'], $response->get_status_description() );
+            $this->assertEquals( $mock_response_array['client_txn_id'], $response->get_client_txn_id() );
+            $this->assertEquals( $mock_response_array['success'], $response->is_success() );
+            $this->assertEquals( $mock_response_array['runtime'], $response->get_runtime() );
+            $this->assertEquals( $mock_response_array['timestamp'], $response->get_timestamp() );
+            $this->assertEquals( $mock_response_array['server_txn_id'], $response->get_server_txn_id() );
+
+            $contact_id_parts = explode( ':', $mock_response_array['data']['contacts']['owner']['contact_id'] );
+
+            $this->assertEquals( $contact_id_parts[0], $response->get_contacts()->get_owner()->get_contact_id()->get_provider_id() );
+            $this->assertEquals( $contact_id_parts[1], $response->get_contacts()->get_owner()->get_contact_id()->get_provider_contact_id() );
 		} catch (\Automattic\Domain_Services\Exception\Domain_Services_Exception $e) {
 			echo 'Exception when calling Domain_Contacts_Set: ', $e->getMessage(), PHP_EOL;
 			var_dump( $e->getCode() );
