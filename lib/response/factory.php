0249943f3a56f64b397442081cc2fbad11625c2b
Del Putnam <del@putnams.net>
2022-11-20 10:26:46 -0500

    Update event details response

--- lib/response/factory.php
+++ lib/response/factory.php
@@ -2,7 +2,7 @@
 
 namespace Automattic\Domain_Services\Response;
 
-use Automattic\Domain_Services\{Command, Exception};
+use Automattic\Domain_Services\{Command, Exception, Response};
 
 class Factory {
 	public function build_response( Command\Command_Interface $command, array $response ): Response_Interface {
@@ -18,12 +18,12 @@ class Factory {
 
 		$class_name = str_replace( '_', '\\', $class_name );
 
-		if ( ! empty( $response['response']['data'] ) && ! is_array( $response['response']['data'] ) ) {
+		if ( !empty( $response['response']['data'] ) && !is_array( $response['response']['data'] ) ) {
 			throw new Exception\Command\Invalid_Format_Exception( 'Response data must be array' );
 		}
 
 		$class_name = __NAMESPACE__ . '\\' . $class_name;
-		if ( ! class_exists( $class_name ) ) {
+		if ( !class_exists( $class_name ) ) {
 			throw new Exception\Domain_Services_Exception( 'Missing response class: ' . $class_name );
 		}
 
