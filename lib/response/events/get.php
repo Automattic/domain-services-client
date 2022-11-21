34e55de7496d32c130c04e061eaca29d140b893a
Del Putnam <del@putnams.net>
2022-11-20 23:20:00 -0500

    Add unit tests for the Events_Get response

--- lib/response/events/get.php
+++ lib/response/events/get.php
@@ -22,14 +22,14 @@ class Get implements Response\Response_Interface {
 	}
 
 	/**
-	 * @return Entity\Event[]
+	 * @return Entity\Reseller_Event[]
 	 */
 	public function get_events(): array {
 		$events_data = $this->get_data_by_key( self::EVENTS );
 
 		$events = [];
 		foreach ( $events_data as $event_data ) {
-			$events[] = Entity\Event::from_array( $event_data );
+			$events[] = Entity\Reseller_Event::from_array( $event_data );
 		}
 
 		return $events;
@@ -42,46 +42,3 @@ class Get implements Response\Response_Interface {
 		return $this->get_data_by_key( self::REQUEST_PARAMS );
 	}
 }
-
-//  'status' => 200,
-//  'status_description' => 'Command completed successfully',
-//  'success' => true,
-//  'client_txn_id' => 'test-client-transaction-id',
-//  'server_txn_id' => '0b91ec36-5539-428f-bf49-1f9f906d2f74.local-isolated-test-request',
-//  'timestamp' => 1667860821,
-//  'runtime' => 0.0008,
-//  'total_count' => 2,
-//  'events' =>
-//  array (
-//	  2 =>
-//		  array (
-//			  'id' => 2,
-//			  'event_class' => 'main-event-class-A',
-//			  'event_subclass' => '',
-//			  'event_data' => '{"test-data":22222}',
-//			  'object_type' => '',
-//			  'object_id' => '',
-//			  'created_date' => '2022-02-01 00:00:00',
-//			  'acknowledged_date' => NULL,
-//		  ),
-//	  3 =>
-//		  array (
-//			  'id' => 3,
-//			  'event_class' => 'main-event-class-B',
-//			  'event_subclass' => '',
-//			  'event_data' => '{"test-data":33333}',
-//			  'object_type' => '',
-//			  'object_id' => '',
-//			  'created_date' => '2022-03-01 00:00:00',
-//			  'acknowledged_date' => NULL,
-//		  ),
-//  ),
-//  'request_params' =>
-//  array (
-//	  'filter' => NULL,
-//	  'first' => 0,
-//	  'limit' => 50,
-//	  'date_start' => NULL,
-//	  'date_end' => NULL,
-//	  'show_acknowledged' => false,
-//  ),
---
+++ test/response/events-get-test.php
@@ -0,0 +1,39 @@
+<?php declare( strict_types=1 );
+
+namespace Automattic\Domain_Services\Test;
+
+use Automattic\Domain_Services\{Api, Command, Configuration, Entity, Mock, Response};
+
+class Events_Get_Test extends Domain_Services_Client_Test_Case {
+	public function test_response_factory_success(): void {
+		$command = new Command\Events\Get();
+
+		$response_data = get_mock_response( $command, 'success' );
+
+		/** @var Response\Events\Get $response_object */
+		$response_object = $this->response_factory->build_response( $command, $response_data );
+
+		// TODO: Add in assertIsValidResponse once this method is merged from PR #5
+
+		$this->assertInstanceOf( Response\Events\Get::class, $response_object );
+
+		$events = $response_object->get_events();
+		$request_params = $response_object->get_request_params();
+		$total_count = $response_object->get_total_count();
+
+		foreach ( $events as $index => $event ) {
+			$this->assertEquals( $response_data['data']['events'][ $index ]['event_data'], json_encode( $event->get_event_data(), JSON_THROW_ON_ERROR ) );
+			$this->assertEquals( $response_data['data']['events'][ $index ]['object_type'], $event->get_object_type() );
+			$this->assertEquals( $response_data['data']['events'][ $index ]['object_id'], $event->get_object_id() );
+			$this->assertEquals( $response_data['data']['events'][ $index ]['event_subclass'], $event->get_event_subclass() );
+			$this->assertEquals( $response_data['data']['events'][ $index ]['event_class'], $event->get_event_class() );
+			$this->assertEquals( $response_data['data']['events'][ $index ]['created_date'], $event->get_created_date()->format( Entity\Entity_Interface::DATE_FORMAT ) );
+			$acknowledge_date = $event->get_acknowledged_date();
+			$acknowledge_date = null === $acknowledge_date ? null : $acknowledge_date->format( Entity\Entity_Interface::DATE_FORMAT );
+			$this->assertEquals( $response_data['data']['events'][ $index ]['acknowledged_date'], $acknowledge_date );
+		}
+
+		$this->assertEquals( $response_data['data']['total_count'], $total_count );
+		$this->assertSame( $response_data['data']['request_params'], $request_params );
+	}
+}
