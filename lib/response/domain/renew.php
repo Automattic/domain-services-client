910710b3e1ecafca40dbf87e8fabb5b0a622e46c
Del Putnam <del@putnams.net>
2022-11-27 15:49:05 -0500

    Add unit test for the Domain_Renew response

--- lib/response/domain/renew.php
+++ lib/response/domain/renew.php
@@ -2,12 +2,13 @@
 
 namespace Automattic\Domain_Services\Response\Domain;
 
-use Automattic\Domain_Services\Response;
+use Automattic\Domain_Services\{Entity, Response};
 
 class Renew implements Response\Response_Interface {
 	use Response\Data_Trait;
 
-	public function get_expiration_date(): ?string {
-		return $this->get_data_by_key( 'data.expiration_date' );
+	public function get_expiration_date(): ?\DateTimeImmutable {
+		$expiration_date = $this->get_data_by_key( 'data.expiration_date' );
+		return null === $expiration_date ? null : \DateTimeImmutable::createFromFormat( Entity\Entity_Interface::DATE_FORMAT, $expiration_date );
 	}
 }
