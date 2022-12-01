bf947b22e3b7305ac5fe86ff9b559185033466c5
Del Putnam <del@putnams.net>
2022-12-01 14:03:40 -0500

    Add unit test for the Domain_Check command (#38)

---
+++ test/command/domain-check-test.php
@@ -0,0 +1,38 @@
+<?php declare( strict_types=1 );
+
+namespace Automattic\Domain_Services\Test\Command;
+
+use Automattic\Domain_Services\{Command, Entity, Test};
+
+class Domain_Check_Test extends Test\Lib\Domain_Services_Client_Test_Case {
+	use Command\Array_Key_Domains_Trait;
+
+	public function test_command_instance_success(): void {
+		$mock_command_data = [
+			Command\Command_Interface::COMMAND => 'Domain_Check',
+			Command\Command_Interface::PARAMS => [
+				self::get_domain_names_array_key() => [
+					'test-domain-name-1.com',
+					'test-domain-name-2.com',
+					'test-domain-name-3.com',
+				],
+			],
+			Command\Command_Interface::CLIENT_TXN_ID => 'client-transaction-info-for-domain-transferlock-set-test-1',
+		];
+
+		$domains = new Entity\Domain_Names(
+			...array_map(
+				static fn( $d ) => new Entity\Domain_Name( $d ),
+				$mock_command_data[ Command\Command_Interface::PARAMS ][ self::get_domain_names_array_key() ]
+			)
+		);
+		$command = new Command\Domain\Check( $domains );
+		$command->set_client_txn_id( $mock_command_data[ Command\Command_Interface::CLIENT_TXN_ID ] );
+
+		$this->assertInstanceOf( Command\Domain\Check::class, $command );
+
+		$actual_command_array = $command->jsonSerialize();
+
+		$this->assertSame( $mock_command_data, $actual_command_array );
+	}
+}
