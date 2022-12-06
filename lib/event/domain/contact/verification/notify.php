db294d35a78c44c7f63cea8bd52e97de98293d1d
Del Putnam <del@putnams.net>
2022-12-06 17:26:46 -0500

    Add stubs for all event classes

---
+++ lib/event/domain/contact/verification/notify.php
@@ -0,0 +1,9 @@
+<?php declare( strict_types=1 );
+
+namespace Automattic\Domain_Services\Event\Domain\Contact\Verification;
+
+use Automattic\Domain_Services\{Event};
+
+class Notify implements Event\Event_Interface {
+	use Event\Data_Trait;
+}
---
+++ lib/event/domain/transferout/success.php
@@ -0,0 +1,9 @@
+<?php declare( strict_types=1 );
+
+namespace Automattic\Domain_Services\Event\Domain\Transferout;
+
+use Automattic\Domain_Services\{Event};
+
+class Success implements Event\Event_Interface {
+	use Event\Data_Trait;
+}
