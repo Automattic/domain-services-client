0bcd83308b3ce33bfcf3ecc7988a85f995aa9348
Del Putnam <del@putnams.net>
2022-12-07 14:55:52 -0500

    Add unit test for the Domain_Delete_Success event

--- lib/event/domain/delete/success.php
+++ lib/event/domain/delete/success.php
@@ -5,5 +5,5 @@ namespace Automattic\Domain_Services\Event\Domain\Delete;
 use Automattic\Domain_Services\{Event};
 
 class Success implements Event\Event_Interface {
-	use Event\Data_Trait;
+	use Event\Data_Trait, Event\Object_Type_Domain_Trait;
 }
