b93e08af20aa1cfaf9b1a7394c9a67798a1af0bc
Del Putnam <del@putnams.net>
2022-12-07 16:00:14 -0500

    Remove contact data from the Domain_Contacts_Set response

--- lib/response/domain/contacts/set.php
+++ lib/response/domain/contacts/set.php
@@ -6,9 +6,4 @@ use Automattic\Domain_Services\{Entity, Response};
 
 class Set implements Response\Response_Interface {
 	use Response\Data_Trait;
-
-	public function get_contacts(): Entity\Domain_Contacts {
-		$contact_data = $this->get_data_by_key( 'data.contacts' ) ?? [];
-		return Entity\Domain_Contacts::from_array( $contact_data );
-	}
 }
--- test/response/domain-contacts-set-test.php
+++ test/response/domain-contacts-set-test.php
@@ -38,20 +38,7 @@ class Domain_Contacts_Set_Test extends Test\Lib\Domain_Services_Client_Test_Case
 			'server_txn_id' => 'bf091ce4-bca4-4f8e-8d65-30f37d13609d.local-isolated-test-request',
 			'timestamp' => 1669075519,
 			'runtime' => 0.0104,
-			'data' => [
-				'contacts' => [
-					'owner' => [
-						'contact_id' => 'SP1:P-ABC1234',
-						'contact_information' => $contact_info,
-						'contact_disclosure' => 'none',
-					],
-					'admin' => [
-						'contact_id' => 'SP1:P-XYZ5678',
-						'contact_information' => $contact_info,
-						'contact_disclosure' => 'none',
-					],
-				],
-			],
+			'data' => [],
 		];
 
 
@@ -61,16 +48,5 @@ class Domain_Contacts_Set_Test extends Test\Lib\Domain_Services_Client_Test_Case
 		$this->assertInstanceOf( Response\Domain\Contacts\Set::class, $response_object );
 
 		$this->assertIsValidResponse( $mock_response_data, $response_object );
-
-		$owner_contact = $response_object->get_contacts()->get_owner();
-		$owner_contact_id = (string) $owner_contact->get_contact_id();
-		$owner_contact_info = $owner_contact->get_contact_information()->to_array();
-		$owner_contact_disclosure = $owner_contact->get_contact_disclosure()->get_disclose_fields();
-
-		$this->assertSame( $mock_response_data['data']['contacts']['owner']['contact_id'], $owner_contact_id );
-		$this->assertSame( $mock_response_data['data']['contacts']['owner']['contact_information'], $owner_contact_info );
-		$this->assertSame( $mock_response_data['data']['contacts']['owner']['contact_disclosure'], $owner_contact_disclosure );
-
-		$this->assertSame( $mock_response_data['data']['contacts'], $response_object->get_contacts()->to_array() );
 	}
 }
