d052f02ba338fdade534ef3373c0590e3acd2e36
Del Putnam <del@putnams.net>
2022-12-07 13:14:00 -0500

    Add the event-specific data to the contact verification events (#52)
    * Add the event-specific data to the contact verification events
    
    * Add docblock comment
    
    * Add unit test
    
    * Add unit test
    
    * Fix whitespace
    
    * Add unit test for Contact_Verification_Request event

--- lib/event/contact/verification/notify.php
+++ lib/event/contact/verification/notify.php
@@ -4,6 +4,18 @@ namespace Automattic\Domain_Services\Event\Contact\Verification;
 
 use Automattic\Domain_Services\{Event};
 
+/**
+ * This event indicates a change in the contact status
+ */
 class Notify implements Event\Event_Interface {
 	use Event\Data_Trait;
+
+	/**
+	 * Returns tha verification status of the contact.
+	 *
+	 * @return bool
+	 */
+	public function is_verified(): bool {
+		return $this->get_data_by_key( 'event_data.verified' ) ?? false;
+	}
 }
