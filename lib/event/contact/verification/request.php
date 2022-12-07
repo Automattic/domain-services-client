0b27fe63b8c2e08322246da1169c234c13094914
Del Putnam <del@putnams.net>
2022-12-07 13:14:00 -0500

    Add the event-specific data to the contact verification events (#52)
    * Add the event-specific data to the contact verification events
    
    * Add docblock comment
    
    * Add unit test
    
    * Add unit test
    
    * Fix whitespace
    
    * Add unit test for Contact_Verification_Request event

--- lib/event/contact/verification/request.php
+++ lib/event/contact/verification/request.php
@@ -4,6 +4,18 @@ namespace Automattic\Domain_Services\Event\Contact\Verification;
 
 use Automattic\Domain_Services\{Event};
 
+/**
+ * This event indicates that a verification request was sent to the contact's email address.
+ */
 class Request implements Event\Event_Interface {
 	use Event\Data_Trait;
+
+	/**
+	 * Returns the email address that the verification request was sent to.
+	 *
+	 * @return string|null
+	 */
+	public function get_email(): ?string {
+		return $this->get_data_by_key( 'event_data.email' );
+	}
 }
---
+++ test/event/contact-verification-request-test.php
@@ -0,0 +1,47 @@
+<?php declare( strict_types=1 );
+
+namespace Automattic\Domain_Services\Test\Event;
+
+use Automattic\Domain_Services\{Command, Event, Response, Test};
+
+class Contact_Verification_Request_Test extends Test\Lib\Domain_Services_Client_Test_Case {
+	public function test_event_success(): void {
+		$command = new Command\Event\Details( 1234 );
+
+		$response_data = [
+			'status' => 200,
+			'status_description' => 'Command completed successfully',
+			'success' => true,
+			'client_txn_id' => 'test-client-transaction-id',
+			'server_txn_id' => '5ffdba44-eeec-4647-9cc4-27cdf8352efc.local-isolated-test-request',
+			'timestamp' => 1669075517,
+			'runtime' => 0.0019,
+			'data' => [
+				'event' => [
+					'id' => 1234,
+					'event_class' => 'Contact_Verification',
+					'event_subclass' => 'Request',
+					'object_type' => 'contact',
+					'object_id' => 'SP1:P-ABC1234',
+					'created_date' => '2022-01-23 12:34:56',
+					'acknowldeged_date' => null,
+					'event_data' => [
+						'email' => 'registrant@example.com',
+						'verification_code' => 'qwerty12345',
+					],
+				],
+			],
+		];
+
+		/** @var Response\Event\Details $response_object */
+		$response_object = $this->response_factory->build_response( $command, $response_data );
+
+		$this->assertIsValidResponse( $response_data, $response_object );
+
+		$event = $response_object->get_event();
+		$this->assertNotNull( $event );
+
+		$this->assertInstanceOf( Event\Contact\Verification\Request::class, $event );
+		$this->assertSame( $response_data['data']['event']['event_data']['email'], $event->get_email() );
+	}
+}
