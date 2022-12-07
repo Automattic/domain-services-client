331501773e57c105a78175b2969b4a698d4ef707
Del Putnam <del@putnams.net>
2022-12-07 08:32:21 -0500

    Remove unused pending events; Add more notification events

--- lib/event/domain/privacy/set/pending.php
+++
@@ -1,9 +0,0 @@
-<?php declare( strict_types=1 );
-
-namespace Automattic\Domain_Services\Event\Domain\Privacy\Set;
-
-use Automattic\Domain_Services\{Event};
-
-class Pending implements Event\Event_Interface {
-	use Event\Data_Trait;
-}
