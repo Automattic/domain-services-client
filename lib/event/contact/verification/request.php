eb078ae620307454196aa7a50c56b5eeb88eb7d1
Del Putnam <del@putnams.net>
2022-12-07 12:35:53 -0500

    Add the event-specific data to the contact verification events

--- lib/event/contact/verification/request.php
+++ lib/event/contact/verification/request.php
@@ -4,6 +4,13 @@ namespace Automattic\Domain_Services\Event\Contact\Verification;
 
 use Automattic\Domain_Services\{Event};
 
+/**
+ * This event indicates that a verification request was sent to the contact's email address.
+ */
 class Request implements Event\Event_Interface {
 	use Event\Data_Trait;
+
+	public function get_email(): ?string {
+		return $this->get_data_by_key( 'event_data.email' );
+	}
 }
