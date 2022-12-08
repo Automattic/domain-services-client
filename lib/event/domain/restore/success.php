88af0324b4eec9b8dc1f32a5992f45e4d22f99a5
Del Putnam <del@putnams.net>
2022-12-08 12:48:44 -0500

    Add unit tests for the Domain_Restore events (#84)

--- lib/event/domain/restore/success.php
+++ lib/event/domain/restore/success.php
@@ -2,8 +2,22 @@
 
 namespace Automattic\Domain_Services\Event\Domain\Restore;
 
-use Automattic\Domain_Services\{Event};
+use Automattic\Domain_Services\{Entity, Event};
 
 class Success implements Event\Event_Interface {
 	use Event\Data_Trait, Event\Object_Type_Domain_Trait;
+
+	public function get_domain_status(): ?string {
+		return $this->get_data_by_key( 'event_data.domain_status' );
+	}
+
+	public function get_expiration_date(): ?\DateTimeInterface {
+		$expiration_date = $this->get_data_by_key( 'event_data.expiration_date' );
+		return null === $expiration_date ? null : \DateTimeImmutable::createFromFormat( Entity\Entity_Interface::DATE_FORMAT, $expiration_date );
+	}
+
+	public function get_renewal_date(): ?\DateTimeInterface {
+		$renewal_date = $this->get_data_by_key( 'event_data.renewal_date' );
+		return null === $renewal_date ? null : \DateTimeImmutable::createFromFormat( Entity\Entity_Interface::DATE_FORMAT, $renewal_date );
+	}
 }
