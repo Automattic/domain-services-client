6ea6bf34d60d11d64b73a1ff2996e54ffe4ec2b2
Del Putnam <del@putnams.net>
2022-12-08 08:13:52 -0500

    Add unit test for the Domain_Notification_Agp event (#67)

--- lib/event/domain/notification/agp.php
+++ lib/event/domain/notification/agp.php
@@ -2,8 +2,18 @@
 
 namespace Automattic\Domain_Services\Event\Domain\Notification;
 
-use Automattic\Domain_Services\{Event};
+use Automattic\Domain_Services\{Entity, Event};
 
 class Agp implements Event\Event_Interface {
 	use Event\Data_Trait, Event\Object_Type_Domain_Trait;
+
+	public function get_agp_end_date(): ?\DateTimeImmutable {
+		$agp_end_date = $this->get_data_by_key( 'event_data.agp_end_date' ) ?? null;
+
+		if ( null === $agp_end_date ) {
+			return null;
+		}
+
+		return \DateTimeImmutable::createFromFormat( Entity\Entity_Interface::DATE_FORMAT, $agp_end_date );
+	}
 }
