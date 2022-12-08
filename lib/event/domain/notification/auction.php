7cab9e13d8681bce8d568ff64df3283efecc493e
Del Putnam <del@putnams.net>
2022-12-08 08:48:12 -0500

    Add unit test for the Domain_Notification_Auction event (#69)

--- lib/event/domain/notification/auction.php
+++ lib/event/domain/notification/auction.php
@@ -2,8 +2,33 @@
 
 namespace Automattic\Domain_Services\Event\Domain\Notification;
 
-use Automattic\Domain_Services\{Event};
+use Automattic\Domain_Services\{Entity, Event};
 
 class Auction implements Event\Event_Interface {
 	use Event\Data_Trait, Event\Object_Type_Domain_Trait;
+
+	/**
+	 * This should be one of PARKED, SUBMITTED, ACTIVE
+	 *
+	 * @return string|null
+	 */
+	public function get_auction_status(): ?string {
+		return $this->get_data_by_key( 'event_data.auction_status' );
+	}
+
+	/**
+	 * This is the date at which the domain will exit the current auction state
+	 *
+	 * @return \DateTimeImmutable|null
+	 */
+	public function get_auction_status_end_date(): ?\DateTimeImmutable {
+		$auction_state_end_date = $this->get_data_by_key( 'event_data.auction_status_end_date' ) ?? null;
+
+		if ( null === $auction_state_end_date ) {
+			return null;
+		}
+
+		return \DateTimeImmutable::createFromFormat( Entity\Entity_Interface::DATE_FORMAT, $auction_state_end_date );
+	}
+
 }
