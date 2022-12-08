28ceccbc19a1721e078b91513e13fd42bbcafdf3
Del Putnam <del@putnams.net>
2022-12-08 13:10:15 -0500

    Add unit tests for the Domain_Transferlock_Set events

--- lib/event/domain/transferlock/set/success.php
+++ lib/event/domain/transferlock/set/success.php
@@ -1,9 +1,13 @@
 <?php declare( strict_types=1 );
 
-namespace Automattic\Domain_Services\Event\Domain\Contacts\Set;
+namespace Automattic\Domain_Services\Event\Domain\Transferlock\Set;
 
 use Automattic\Domain_Services\{Event};
 
 class Success implements Event\Event_Interface {
 	use Event\Data_Trait, Event\Object_Type_Domain_Trait;
+
+	public function is_locked(): bool {
+		return $this->get_data_by_key( 'event_data.transferlock' ) ?? false;
+	}
 }
