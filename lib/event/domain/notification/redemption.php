376ade622a57fc266a0103156893299218aee4e9
Del Putnam <del@putnams.net>
2022-12-08 10:18:00 -0500

    Add unit test for the Domain_Notification_Redemption event (#72)

--- lib/event/domain/notification/redemption.php
+++ lib/event/domain/notification/redemption.php
@@ -2,8 +2,18 @@
 
 namespace Automattic\Domain_Services\Event\Domain\Notification;
 
-use Automattic\Domain_Services\{Event};
+use Automattic\Domain_Services\{Entity, Event};
 
 class Redemption implements Event\Event_Interface {
 	use Event\Data_Trait, Event\Object_Type_Domain_Trait;
+
+	public function get_redemption_end_date(): ?\DateTimeImmutable {
+		$redemption_end_date = $this->get_data_by_key( 'event_data.redemption_end_date' );
+
+		if ( null === $redemption_end_date ) {
+			return null;
+		}
+
+		return \DateTimeImmutable::createFromFormat( Entity\Entity_Interface::DATE_FORMAT, $redemption_end_date );
+	}
 }
