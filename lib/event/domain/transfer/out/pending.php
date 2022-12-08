b633d908a081ae4b8efc899b428ff43d12fe2a3e
Del Putnam <del@putnams.net>
2022-12-08 07:58:08 -0500

    Add appropriate event traits to the event classes (#66)

--- lib/event/domain/transfer/out/pending.php
+++ lib/event/domain/transfer/out/pending.php
@@ -5,5 +5,5 @@ namespace Automattic\Domain_Services\Event\Domain\Transfer\Out;
 use Automattic\Domain_Services\{Event};
 
 class Pending implements Event\Event_Interface {
-	use Event\Data_Trait;
+	use Event\Data_Trait, Event\Object_Type_Domain_Trait;
 }
