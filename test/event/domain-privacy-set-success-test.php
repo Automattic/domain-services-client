1472a01c38810b65e7902c1d1aa537defa38b258
Del Putnam <del@putnams.net>
2022-12-08 11:41:56 -0500

    Add unit test for the Domain_Privacy_Set_Success event (#77)
    * Add unit test for the Domain_Privacy_Set_Success event
    
    * Import exception namespace

---
+++ test/event/domain-privacy-set-success-test.php
@@ -0,0 +1,47 @@
+<?php declare( strict_types=1 );
+
+namespace Automattic\Domain_Services\Test\Event;
+
+use Automattic\Domain_Services\{Command, Event, Response, Test};
+
+class Domain_Privacy_Set_Success_Test extends Test\Lib\Domain_Services_Client_Test_Case {
+	public function test_event_success(): void {
+		$command = new Command\Event\Details( 1234 );
+
+		$response_data = [
+			'status' => 200,
+			'status_description' => 'Command completed successfully',
+			'success' => true,
+			'client_txn_id' => 'test-client-transaction-id',
+			'server_txn_id' => '5ffdba44-eeec-4647-9cc4-27cdf8352efc.local-isolated-test-request',
+			'timestamp' => 1669075517,
+			'runtime' => 0.0019,
+			'data' => [
+				'event' => [
+					'id' => 1234,
+					'event_class' => 'Domain_Privacy_Set',
+					'event_subclass' => 'Success',
+					'object_type' => 'domain',
+					'object_id' => 'example.com',
+					'event_date' => '2022-01-23 12:34:56',
+					'acknowledged_date' => null,
+					'event_data' => [
+						'privacy_setting' => 'enable_privacy_service',
+					],
+				],
+			],
+		];
+
+		/** @var Response\Event\Details $response_object */
+		$response_object = $this->response_factory->build_response( $command, $response_data );
+
+		$this->assertIsValidResponse( $response_data, $response_object );
+
+		$event = $response_object->get_event();
+		$this->assertNotNull( $event );
+
+		$this->assertInstanceOf( Event\Domain\Privacy\Set\Success::class, $event );
+		$this->assertSame( $response_data['data']['event']['event_data']['privacy_setting'], $event->get_privacy_setting()->get_setting() );
+		$this->assertSame( $response_data['data']['event']['object_id'], $event->get_domain()->get_name() );
+	}
+}
