37ce9c7476b554c328d068a9970e040e6f51d4bf
Del Putnam <del@putnams.net>
2022-12-08 11:41:56 -0500

    Add unit test for the Domain_Privacy_Set_Success event (#77)
    * Add unit test for the Domain_Privacy_Set_Success event
    
    * Import exception namespace

--- lib/event/domain/privacy/set/success.php
+++ lib/event/domain/privacy/set/success.php
@@ -2,8 +2,24 @@
 
 namespace Automattic\Domain_Services\Event\Domain\Privacy\Set;
 
-use Automattic\Domain_Services\{Event};
+use Automattic\Domain_Services\{Entity, Event, Exception};
 
 class Success implements Event\Event_Interface {
 	use Event\Data_Trait, Event\Object_Type_Domain_Trait;
+
+	/**
+	 * Returns the Whois_Privacy setting that for this domain, if available
+	 *
+	 * @return Entity\Whois_Privacy|null
+	 * @throws Exception\Entity\Invalid_Value_Exception
+	 */
+	public function get_privacy_setting(): ?Entity\Whois_Privacy {
+		$privacy_setting_data = $this->get_data_by_key( 'event_data.privacy_setting' );
+
+		if ( null === $privacy_setting_data ) {
+			return null;
+		}
+
+		return new Entity\Whois_Privacy( $privacy_setting_data );
+	}
 }
