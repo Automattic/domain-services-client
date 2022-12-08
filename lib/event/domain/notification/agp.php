e99fa7c48d5f18f40d76ddc80a3a2f4fcd7a6b80
Del Putnam <del@putnams.net>
2022-12-08 07:58:08 -0500

    Add appropriate event traits to the event classes (#66)

--- lib/event/domain/notification/agp.php
+++ lib/event/domain/notification/agp.php
@@ -5,5 +5,5 @@ namespace Automattic\Domain_Services\Event\Domain\Notification;
 use Automattic\Domain_Services\{Event};
 
 class Agp implements Event\Event_Interface {
-	use Event\Data_Trait;
+	use Event\Data_Trait, Event\Object_Type_Domain_Trait;
 }
--- lib/event/domain/notification/suspended.php
+++ lib/event/domain/notification/suspended.php
@@ -5,5 +5,5 @@ namespace Automattic\Domain_Services\Event\Domain\Notification;
 use Automattic\Domain_Services\{Event};
 
 class Suspended implements Event\Event_Interface {
-	use Event\Data_Trait;
+	use Event\Data_Trait, Event\Object_Type_Domain_Trait;
 }
