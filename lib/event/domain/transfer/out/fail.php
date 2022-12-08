462d1a603e63b26f0a242f15127893e7ff83ccd9
Del Putnam <del@putnams.net>
2022-12-08 13:43:22 -0500

    Move all of the transfer-related data access methods to a trait

--- lib/event/domain/transfer/out/fail.php
+++ lib/event/domain/transfer/out/fail.php
@@ -5,5 +5,5 @@ namespace Automattic\Domain_Services\Event\Domain\Transfer\Out;
 use Automattic\Domain_Services\{Event};
 
 class Fail implements Event\Event_Interface {
-	use Event\Data_Trait, Event\Object_Type_Domain_Trait;
+	use Event\Data_Trait, Event\Object_Type_Domain_Trait, Event\Transfer_Trait;
 }
