893caa7ca994af1ec7c013830d61bb43b9a8adf8
Del Putnam <del@putnams.net>
2022-12-08 08:23:35 -0500

    Add unit test for the Domain_Notification_Argp event (#68)

--- lib/event/domain/notification/argp.php
+++ lib/event/domain/notification/argp.php
@@ -2,8 +2,18 @@
 
 namespace Automattic\Domain_Services\Event\Domain\Notification;
 
-use Automattic\Domain_Services\{Event};
+use Automattic\Domain_Services\{Entity, Event};
 
 class Argp implements Event\Event_Interface {
 	use Event\Data_Trait, Event\Object_Type_Domain_Trait;
+
+	public function get_argp_end_date(): ?\DateTimeImmutable {
+		$argp_end_date = $this->get_data_by_key( 'event_data.argp_end_date' ) ?? null;
+
+		if ( null === $argp_end_date ) {
+			return null;
+		}
+
+		return \DateTimeImmutable::createFromFormat( Entity\Entity_Interface::DATE_FORMAT, $argp_end_date );
+	}
 }
