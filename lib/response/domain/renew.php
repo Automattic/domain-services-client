d7d64d989423d8aeb9ee2c4e32b6847c3317f65c
Del Putnam <del@putnams.net>
2022-12-08 12:53:13 -0500

    Remove the response data from the Domain_Renew response (#85)

--- lib/response/domain/renew.php
+++ lib/response/domain/renew.php
@@ -6,9 +6,4 @@ use Automattic\Domain_Services\{Entity, Response};
 
 class Renew implements Response\Response_Interface {
 	use Response\Data_Trait;
-
-	public function get_expiration_date(): ?\DateTimeImmutable {
-		$expiration_date = $this->get_data_by_key( 'data.expiration_date' );
-		return null === $expiration_date ? null : \DateTimeImmutable::createFromFormat( Entity\Entity_Interface::DATE_FORMAT, $expiration_date );
-	}
 }
