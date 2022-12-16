2d6576a5bcc109acb4cd7aefd341f828cbcef1a1
Del Putnam <del@putnams.net>
2022-12-16 13:18:40 -0500

    Add the GPLv2 license and copyright notices to all files.

--- lib/loader.php
+++ lib/loader.php
@@ -1,11 +1,26 @@
 <?php declare( strict_types=1 );
+/*
+ * Copyright (c) 2022 Automattic, Inc.
+ *
+ * This file is part of the Automattic Domain Services Client library.
+ *
+ * The Automattic Domain Services Client library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, see https://www.gnu.org/licenses.
+ */
 
 /**
  * Domain Services API Autoloader
  *
  * Modified from code found here: https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-4-autoloader-examples.md
  */
-
 spl_autoload_register(
 	static function ( $class ) {
 
