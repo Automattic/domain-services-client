6999024676c87470a1cbd037b76dc323b87ef3ea
Ramy Nasr <ramy.nasr@automattic.com>
2022-12-19 13:23:48 -0500

    Event details docs

--- lib/response/event/details.php
+++ lib/response/event/details.php
@@ -22,14 +22,21 @@ use Automattic\Domain_Services\{Event, Exception\Event\Invalid_Event_Name, Respo
 
 /**
  * Response of an Event_Details command.
+ *
+ * This is the response returned from a successful execution of Event_Details command.The event can be retrieved using get_event() method.
+ *
+ * @see \Automattic\Domain_Services\Command\Event\Details
  */
 class Details implements Response\Response_Interface {
 	use Response\Data_Trait, Response\Event_Aware;
 
 	/**
+	 * Builds and returns an event entity
+	 *
+	 * @return Event\Event_Interface
 	 * @throws Invalid_Event_Name
 	 */
-	public function get_event(): ?Event\Event_Interface {
+	public function get_event(): Event\Event_Interface {
 		$event_data = $this->get_data_by_key( 'data.event' );
 
 		return $this->get_event_factory()->build_event( $event_data );
