c52b477b0988880de8f893205abf098d427376af
Del Putnam <del@putnams.net>
2022-12-20 15:39:17 -0500

    Domain\Info command and response doc udpates

--- lib/command/domain/info.php
+++ lib/command/domain/info.php
@@ -21,11 +21,12 @@ namespace Automattic\Domain_Services\Command\Domain;
 use Automattic\Domain_Services\{Command, Entity};
 
 /**
- * Retrieves information about a domain that is registered with us.
+ * Retrieves information about a domain that is registered with the reseller.
  *
- * Retrieves various information about a domain that is registered with us, such as creation and expiry dates, auth code,
- * name servers and EPP statuses. If the domain is not registered with us, the information of a Domain_Check command
- * is returned instead (domain availability and fees).
+ * - This command retrieves information about a domain that is registered on the reseller's account.
+ * - If the domain is not registered on the reseller's account an error is returned.
+ *
+ * @see \Automattic\Domain_Services\Response\Domain\Info
  */
 class Info implements Command\Command_Interface, Command\Command_Serialize_Interface {
 	use Command\Command_Trait, Command\Command_Serialize_Trait, Command\Array_Key_Domain_Trait;
@@ -35,18 +36,34 @@ class Info implements Command\Command_Interface, Command\Command_Serialize_Inter
 	 */
 	private Entity\Domain_Name $domain;
 
+	/**
+	 * Domain\Info command constructor
+	 *
+	 * @param Entity\Domain_Name $domain
+	 */
 	public function __construct( Entity\Domain_Name $domain ) {
 		$this->domain = $domain;
 	}
 
+	/**
+	 * Gets the domain name
+	 *
+	 * @return Entity\Domain_Name
+	 */
 	public function get_domain(): Entity\Domain_Name {
 		return $this->domain;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public static function get_name(): string {
 		return 'Domain_Info';
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public function to_array(): array {
 		return [
 			self::get_domain_name_array_key() => $this->get_domain()->get_name(),
