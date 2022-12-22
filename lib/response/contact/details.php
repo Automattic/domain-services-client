0b1d9f6b930a14e080677aa4981fccda4be5342c
Ramy Nasr <ramy.nasr@automattic.com>
2022-12-22 17:05:10 -0500

    apply PR requests

--- lib/response/contact/details.php
+++ lib/response/contact/details.php
@@ -21,7 +21,7 @@ namespace Automattic\Domain_Services\Response\Contact;
 use Automattic\Domain_Services\{Entity, Response, Exception};
 
 /**
- * Response containing the Contact_Information associated with a Contact_Id.
+ * Response containing the Contact_Information associated with a Contact_Id
  *
  * @see \Automattic\Domain_Services\Command\Contact\Details
  */
