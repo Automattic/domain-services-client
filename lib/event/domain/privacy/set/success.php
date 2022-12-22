a46091f3c7062a4432428817fa9baaeddd8d6c7e
Igor Giussani <igor.giussani@automattic.com>
2022-12-22 15:34:44 +0100

    Update docs for `Domain\Privacy\Set` command, response and events (#121)
    * Update docs for domain privacy set command, response and events

--- lib/event/domain/privacy/set/success.php
+++ lib/event/domain/privacy/set/success.php
@@ -20,11 +20,19 @@ namespace Automattic\Domain_Services\Event\Domain\Privacy\Set;
 
 use Automattic\Domain_Services\{Entity, Event, Exception};
 
+/**
+ * Success event for a `Domain\Privacy\Set command
+ *
+ * - This event is generated when a privacy setting update succeds at the server.
+ * - Contains a `privacy_setting` property with the privacy option that was set at the registry
+ *
+ * @see \Automattic\Domain_Services\Command\Domain\Privacy\Set
+ */
 class Success implements Event\Event_Interface {
 	use Event\Data_Trait, Event\Object_Type_Domain_Trait;
 
 	/**
-	 * Returns the Whois_Privacy setting that for this domain, if available
+	 * Returns the Whois_Privacy setting that was set for this domain
 	 *
 	 * @return Entity\Whois_Privacy|null
 	 * @throws Exception\Entity\Invalid_Value_Exception
