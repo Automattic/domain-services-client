0940289294c896584770fb29afe7eab568657250
Ramy Nasr <ramy.nasr@automattic.com>
2023-01-12 13:20:08 -0500

    remove provider ID logic from Contact_Id

--- lib/entity/entity-interface.php
+++ lib/entity/entity-interface.php
@@ -20,10 +20,4 @@ namespace Automattic\Domain_Services\Entity;
 
 interface Entity_Interface {
 	public const DATE_FORMAT = 'Y-m-d H:i:s';
-
-	public const SP1 = 'SP1';
-
-	public const VALID_PROVIDER_IDS = [
-		self::SP1,
-	];
 }
