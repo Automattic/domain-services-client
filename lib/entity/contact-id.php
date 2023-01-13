3bcd9d1e744e51f94cccfa420856c5c0c8fd81d6
Ramy Nasr <ramy.nasr@automattic.com>
2023-01-13 13:44:59 -0500

    remove unused server code from entity classes (#147)

--- lib/entity/contact-id.php
+++ lib/entity/contact-id.php
@@ -29,8 +29,10 @@ use Automattic\Domain_Services\{Exception};
  * @see Domain_Contact
  */
 class Contact_Id implements Entity_Interface {
-	private string $provider_id;
-	private string $provider_contact_id;
+	/**
+	 * @var string
+	 */
+	private string $contact_id;
 
 	/**
 	 * Constructs a Contact_Id entity
@@ -46,37 +48,17 @@ class Contact_Id implements Entity_Interface {
 			throw new Exception\Entity\Invalid_Value_Exception( __CLASS__, 'Invalid contact ID format' );
 		}
 
-		$this->provider_id = $contact_id_parts[0];
-		$this->provider_contact_id = $contact_id_parts[1];
-
-		if ( ! in_array( $this->provider_id, Entity_Interface::VALID_PROVIDER_IDS, true ) ) {
-			throw new Exception\Entity\Invalid_Value_Exception( __CLASS__, 'Invalid contact ID prefix' );
-		}
-	}
-
-	private static function build_string( string $provider_id, string $provider_contact_id ): string {
-		return $provider_id . ':' . $provider_contact_id;
+		$this->contact_id = $contact_id;
 	}
 
 	public function __toString(): string {
-		return self::build_string( $this->provider_id, $this->provider_contact_id );
-	}
-
-	public static function build_for_provider( string $provider_id, string $provider_contact_id ): self {
-		return new self( self::build_string( $provider_id, $provider_contact_id ) );
+		return $this->get_contact_id();
 	}
 
 	/**
 	 * @return string
 	 */
-	public function get_provider_id(): string {
-		return $this->provider_id;
-	}
-
-	/**
-	 * @return string
-	 */
-	public function get_provider_contact_id(): string {
-		return $this->provider_contact_id;
+	public function get_contact_id(): string {
+		return $this->contact_id;
 	}
 }
